5a358c877ca368b017e0bcfe782604de6ca61990
twtjudy1128 <68011522+twtjudy1128@users.noreply.github.com>
2020-09-13 09:13:02 +0900

    Create Card.vue

---
+++ Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="column">
+    <div class="card">
+      <header class="headercolor card-header">
+        <p class="card-header-title has-text-grey">
+          {{ title }}
+        </p>
+      </header>
+      <div class="card-content">        
+        <figure class="image is-square">
+          <img v-bind:src="url"/>
+        </figure>
+      </div>
+      <footer class="card-footer">
+        <div class="card-footer-item">
+          <span>
+            <slot />
+          </span>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.headercolor{
+  background-color:gold;
+}
+</style>
